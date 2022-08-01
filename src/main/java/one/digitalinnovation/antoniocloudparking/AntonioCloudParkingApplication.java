7cb4e277b24e837630be6835e34a47e963c7a08d
toninho21 <toninho21@uol.com.br>
2022-08-01 19:11:46 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/antoniocloudparking/AntonioCloudParkingApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.antoniocloudparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntonioCloudParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntonioCloudParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/antoniocloudparking/AntonioCloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.antoniocloudparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntonioCloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
